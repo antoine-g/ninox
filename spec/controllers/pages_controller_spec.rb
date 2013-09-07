07e57c46aae1c1d4874c59bdec605ac6015bebe8
Antoine Giraudmaillet <antoine-g@live.fr>
2013-09-07 13:49:44 +0200

    Add a main title + tests failing

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  before(:each) do
+    @base_title = "Ninox | "
+  end
+
   it "GET on '/' should route to GET 'home'" do
     { :get => "/" }.should route_to(:controller => "pages", :action => "home")
   end
@@ -15,6 +19,10 @@ describe PagesController do
     it "should be success" do
       response.should be_success
     end
+
+    it "should have the right title" do
+      response.should have_selector("title", :content => @base_title + "Home")
+    end
   end
 
   describe "GET 'about'" do
@@ -24,6 +32,10 @@ describe PagesController do
 
     it "should be success" do
       response.should be_success
+    end
+
+    it "should have the right title" do
+      response.should have_selector("title", :content => @base_title + "About")
     end
   end
 
