d5da5ea2f4fe8d9fa337ed940426ea7487a39c2d
Antoine Giraudmaillet <antoine-g@live.fr>
2013-09-07 11:16:05 +0200

    User model created

---
+++ db/migrate/20130907080850_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email 
+      t.string :password_hash
+      t.string :salt
+      t.boolean :admin
+
+      t.timestamps
+    end
+
+    add_index :users, :email, :unique => true
+  end
+end
