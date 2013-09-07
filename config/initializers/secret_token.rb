2045a010e7bf0dfd64915f106fa9bb589ae6747d
Antoine Giraudmaillet <antoine-g@live.fr>
2013-09-07 09:45:14 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ninox::Application.config.secret_token = '7ac9eb4e9f3d2ccdd1a59045b2c0a7c541c593b16228484522db8b0b8c9fa1a05315b8186ae9fb2c0bc536df04ca7f004c1f239ae4f9acfc57a7dbe077e58784'
