30b5b5800e728b1f6284167e47d72f1bb9c54d37
Antoine Giraudmaillet <antoine-g@live.fr>
2013-09-07 19:26:11 +0200

    User controller : creation of the action show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user  = User.find(params[:id])
-    @title = User.email
+    @title = @user.email
   end
 
   def new
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -10,6 +10,27 @@ describe UsersController do
     end
   end
 
+  describe "GET 'show'" do
+    before(:each) do
+      @user = FactoryGirl.create(:user)
+    end
+
+    it "should return http success" do
+      get 'show', :id => @user
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'show', :id => @user
+      response.should have_selector(:title => "#{@app_name} | #{@user.email}")
+    end
+
+    it "should display the right user" do
+      get :show, :id => @user
+      assigns(:user).should == @user
+    end
+  end
+
   describe "GET 'new'" do
     it "should return http success" do
       get 'new'
