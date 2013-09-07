403a05f2c50ac3fa6098e8cf6b40d433a80c5624
Antoine Giraudmaillet <antoine-g@live.fr>
2013-09-07 18:47:22 +0200

    Simple Rspec test on index and new actions for User

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe UsersController do
+  render_views
+
+  describe "GET 'index'" do
+    it "returns http success" do
+      get 'index'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'new'" do
+    it "should return http success" do
+      get 'new'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'new'
+      response.should have_selector(:title => "#{@app_name} | #{title}")
+    end
+  end
+end
