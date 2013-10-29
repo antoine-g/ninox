749eb22aabf5027dcfb0ac2ef6e984fb827e0e97
Antoine Giraudmaillet <antoine-g@live.fr>
2013-10-29 01:15:11 +0100

    Add document model & views

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,4 +2,8 @@ class Document < ActiveRecord::Base
   attr_accessible :desc, :title, :course_id, :user_id
   belongs_to :course
   belongs_to :user
+  has_attached_file :docfile
+
+  validates_presence_of :user
+  validates_presence_of :title
 end
